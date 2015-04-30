2b6c550f895974639ba0150b82e406beea318d30
Urbain Vaes <urbain@vaes.uk>
2015-04-30 18:53:43 +0100

    Added file with templates

---
+++ code/finite/templates.hpp
@@ -0,0 +1,30 @@
+#ifndef TEMPLATES_H
+#define TEMPLATES_H
+
+#include <vector>
+
+template<class type> std::vector<type> operator-(const std::vector<type>& v1, const std::vector<type>& v2) {
+    std::vector<type> result(v1.size());
+    for (unsigned int i = 0; i < v1.size(); ++i) {
+        result[i] = v1[i] - v2[i];
+    }
+    return result;
+}
+
+template<class type> std::vector<type> operator+(const std::vector<type>& v1, const std::vector<type>& v2) {
+    std::vector<type> result(v1.size());
+    for (unsigned int i = 0; i < v1.size(); ++i) {
+        result[i] = v1[i] + v2[i];
+    }
+    return result;
+}
+
+template<class type> double fabs(std::vector<type> vec) {
+    double result = 0.;
+    for (unsigned int i = 0; i < vec.size(); ++i) {
+        result += fabs(vec[i]);
+    }
+    return result;
+}
+
+#endif
