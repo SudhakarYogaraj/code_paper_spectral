6892b857bce39e2a4d89bb191716a58f9edbeaf4
Urbain Vaes <urbain@vaes.uk>
2015-05-01 17:00:40 +0100

    Removed bug for the nonpolynomial case. Working version

--- code/finite/aux.cpp
+++ code/finite/aux.cpp
@@ -77,7 +77,6 @@ double hermite(int n, double x, double sigma) {
         cout << "Variance has to be positive, but sigma = " << sigma <<  endl;
         exit(0);
     }
-
     switch (n) {
         case 0: toReturn = 1.0; break;
         case 1: toReturn = x; break;
@@ -95,11 +94,6 @@ double hermite(int n, double x, double sigma) {
         case 13: toReturn = 5.479963503528103E1*x*((x*x)*-2.7027E5+(x*x*x*x)*1.35135E5-(x*x*x*x*x*x)*2.574E4+(x*x*x*x*x*x*x*x)*2.145E3-pow(x,1.0E1)*7.8E1+pow(x,1.2E1)+1.35135E5)*2.312502312502313E-7; break;
         case 14: toReturn = sqrt(8.58E2)*((x*x)*9.45945E5-(x*x*x*x)*9.45945E5+(x*x*x*x*x*x)*3.15315E5-(x*x*x*x*x*x*x*x)*4.5045E4+pow(x,1.0E1)*3.003E3-pow(x,1.2E1)*9.1E1+pow(x,1.4E1)-1.35135E5)*1.156251156251156E-7; break;
         case 15: toReturn = 3.781534080237807E1*x*((x*x)*4.729725E6-(x*x*x*x)*2.837835E6+(x*x*x*x*x*x)*6.75675E5-(x*x*x*x*x*x*x*x)*7.5075E4+pow(x,1.0E1)*4.095E3-pow(x,1.2E1)*1.05E2+pow(x,1.4E1)-2.027025E6)*2.312502312502313E-8; break;
-        case 16: toReturn = 3.781534080237807E1*((x*x)*-1.62162E7+(x*x*x*x)*1.89189E7-(x*x*x*x*x*x)*7.56756E6+(x*x*x*x*x*x*x*x)*1.35135E6-pow(x,1.0E1)*1.2012E5+pow(x,1.2E1)*5.46E3-pow(x,1.4E1)*1.2E2+pow(x,1.6E1)+2.027025E6)*5.781255781255781E-9; break;
-        case 17: toReturn = 1.559166443969341E2*x*((x*x)*-9.18918E7+(x*x*x*x)*6.432426E7-(x*x*x*x*x*x)*1.837836E7+(x*x*x*x*x*x*x*x)*2.55255E6-pow(x,1.0E1)*1.8564E5+pow(x,1.2E1)*7.14E3-pow(x,1.4E1)*1.36E2+pow(x,1.6E1)+3.4459425E7)*3.400738694856342E-10; break;
-        case 18: toReturn = 1.102497165529236E2*((x*x)*3.10134825E8-(x*x*x*x)*4.135131E8+(x*x*x*x*x*x)*1.9297278E8-(x*x*x*x*x*x*x*x)*4.135131E7+pow(x,1.0E1)*4.59459E6-pow(x,1.2E1)*2.7846E5+pow(x,1.4E1)*9.18E3-pow(x,1.6E1)*1.53E2+pow(x,1.8E1)-3.4459425E7)*1.133579564952114E-10; break;
-        case 19: toReturn = 4.805673730081975E2*x*((x*x)*1.964187225E9-(x*x*x*x)*1.57134978E9+(x*x*x*x*x*x)*5.2378326E8-(x*x*x*x*x*x*x*x)*8.729721E7+pow(x,1.0E1)*7.93611E6-pow(x,1.2E1)*4.0698E5+pow(x,1.4E1)*1.1628E4-pow(x,1.6E1)*1.71E2+pow(x,1.8E1)-6.54729075E8)*5.966208236590074E-12; break;
-        case 20: toReturn = 2.149162627629654E2*((x*x)*-6.54729075E9+(x*x*x*x)*9.820936125E9-(x*x*x*x*x*x)*5.2378326E9+(x*x*x*x*x*x*x*x)*1.30945815E9-pow(x,1.0E1)*1.7459442E8+pow(x,1.2E1)*1.322685E7-pow(x,1.4E1)*5.814E5+pow(x,1.6E1)*1.4535E4-pow(x,1.8E1)*1.9E2+pow(x,2.0E1)+6.54729075E8)*2.983104118295037E-12; break;
         default: cout << "Degree too high" << endl; exit(0);
     }
     return toReturn;
