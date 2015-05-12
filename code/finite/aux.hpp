a714b734438a56532a0988fc2a3c5869280c91a9
Urbain Vaes <u.vaes13@imperial.ac.uk>
2015-05-12 13:28:57 +0100

    first implementation of linear system

--- code/finite/aux.hpp
+++ code/finite/aux.hpp
@@ -12,6 +12,7 @@ std::vector< std::vector<double> > symmetric(std::vector< std::vector<double> > 
 std::vector< std::vector<double> > cholesky(std::vector< std::vector<double> > A);
 double gaussian(std::vector<double> y, std::vector<double> sigmas);
 double monomial(std::vector<int> mult, std::vector<double> x, std::vector<double> sigmas);
+double monomial(std::vector<int> mult, std::vector<double> x);
 std::vector<double> mon2herm (std::vector<double> mcoeffs, int n, int d);
 int mult2ind(std::vector<int> m, int d);
 std::vector<int> ind2mult(int ind, int d, int n);
