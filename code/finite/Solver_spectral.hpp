c901e7a263387d07d2911d358e6c9629d896c776
Urbain Vaes <urbain@vaes.uk>
2015-05-13 16:22:19 +0100

    implemented generic basis

--- code/finite/Solver_spectral.hpp
+++ code/finite/Solver_spectral.hpp
@@ -16,14 +16,23 @@ class Solver_spectral {
         void estimator(Problem&, std::vector<double> x, std::vector<SDE_coeffs>& , double t);
 
     private:
-        std::vector< std::vector<double> > hermiteCoeffs;
+        // Matrix containing the coefficients of uni- and multi-
+        // dimensional Hermite polynomials in terms of monomials.
+        std::vector< std::vector<double> > hermiteCoeffs_1d;
+        std::vector< std::vector<double> > hermiteCoeffs_nd;
+
+        // Basis used for the method
         double basis(std::vector<int> mult, std::vector<double> x, std::vector<double> sigmas);
 
-        /* 
-         * Function that returns the vector of standard (normalized with weight of variance 1) hermite coefficients, based on the monomial expression
-         */
+        // Function to obtain the coefficients of the expansion of a
+        // polynomial in terms of hermite polynomials, based on its
+        // exansion in monomials.
         std::vector<double> mon2herm (std::vector<double> mcoeffs, int n, int d);
+
+        // Auxiliary function to convert multi-indices to linear indices.
         int mult2ind(std::vector<int> m, int d);
+
+        // Auxiliary function to convert linear indices to multi-indices.
         std::vector<int> ind2mult(int ind, int d, int n);
 };
 #endif
