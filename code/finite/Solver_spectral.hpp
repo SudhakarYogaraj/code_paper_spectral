0b15229645939238a1ce2af815ffec9292e57df3
Urbain Vaes <u.vaes13@imperial.ac.uk>
2015-05-20 21:11:36 +0100

    working version with 1 bug (if one uses exact sigma)

--- code/finite/Solver_spectral.hpp
+++ code/finite/Solver_spectral.hpp
@@ -20,6 +20,7 @@ class Solver_spectral {
         // dimensional Hermite polynomials in terms of monomials.
         std::vector< std::vector<double> > hermiteCoeffs_1d;
         std::vector< std::vector<double> > hermiteCoeffs_nd;
+        std::vector< std::vector<double> > herm_to_basis;
 
         // Basis used for the method
         double basis(std::vector<int> mult, std::vector<double> x, std::vector<double> sigmas);
@@ -27,12 +28,15 @@ class Solver_spectral {
         // Function to obtain the coefficients of the expansion of a
         // polynomial in terms of hermite polynomials, based on its
         // exansion in monomials.
-        std::vector<double> mon2herm (std::vector<double> mcoeffs, int n, int d);
+        std::vector<double> basis2herm (std::vector<double> mcoeffs, int n, int d);
 
         // Auxiliary function to convert multi-indices to linear indices.
         int mult2ind(std::vector<int> m, int d);
 
         // Auxiliary function to convert linear indices to multi-indices.
         std::vector<int> ind2mult(int ind, int d, int n);
+
+        //
+        int monomial = 1;
 };
 #endif
