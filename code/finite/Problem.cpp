e4b6d3d347765af005a003795bcafa169c56d8c7
Urbain Vaes <u.vaes13@imperial.ac.uk>
2015-05-25 12:41:10 +0100

    factorized build_problem_nd and build_problem_1d into 1

--- code/finite/Problem.cpp
+++ code/finite/Problem.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void Problem::init() {
 
     this->t_end = 1.;
-    this->nf = 1;
+    this->nf = 2;
     this->d = 1;
     this->x0 = vector<double>(d,1.2);
     this->lambdas = {2};
@@ -96,7 +96,7 @@ vector< vector<double> > Problem::phi_x(vector<double> x, vector<double> y) {
 
 vector< vector<double> > Problem::dax(vector<double> x, vector<double> y) {
     vector< vector<double> > result(this->d,vector<double>(this->d,0.));
-    result[0][0] = -sin(x[0])*(sin(y[0])+y[0]*cos(y[0])*2.0);
+    result[0][0] = -sin(x[0])*(sin(y[0])+(y[0]*y[0]*y[0])*cos(y[0])-y[0]*cos(y[0]));
     return result;
 }
 
@@ -108,7 +108,7 @@ vector< vector<double> > Problem::grad_h(vector<double> x, vector<double> y) {
 
 vector<double> Problem::grad(vector<double> x, vector<double> y){
     vector<double> result(this->nf);
-    result[0] = y[0]*2.0;
+    result[0] = -y[0]+y[0]*y[0]*y[0];
     return result;
 }
 
@@ -120,7 +120,7 @@ vector<double> Problem::phi(vector<double> x, vector<double> y) {
 
 vector<double> Problem::a(vector<double> x, vector<double> y) {
     vector<double> result(this->d,0.);
-    result[0] = cos(x[0])*(sin(y[0])+y[0]*cos(y[0])*2.0);
+    result[0] = cos(x[0])*(sin(y[0])+(y[0]*y[0]*y[0])*cos(y[0])-y[0]*cos(y[0]));
     return result;
 }
 
@@ -132,32 +132,32 @@ vector<double> Problem::fast_drift_h(vector<double> x, vector<double> y) {
 
 double Problem::potential(vector<double> x, vector<double> y) {
     double result = 0.;
-    result = log(3.141592653589793)*(1.0/2.0)+y[0]*y[0];
+    result = (y[0]*y[0])*(-1.0/2.0)+(y[0]*y[0]*y[0]*y[0])*(1.0/4.0);
     return result;
 }
 
 double Problem::linearTerm(vector<double> x, vector<double> y){
     double result;
-    result = -y[0]*y[0]+1.0;
+    result = pow(y[0]-y[0]*y[0]*y[0],2.0)*(-1.0/4.0)+(y[0]*y[0])*(3.0/2.0)-1.0/2.0;
     return result;
 }
 
 double Problem::rho(vector<double> x, vector<double> y) {
     double result = 0.;
-    result = 1.0/sqrt(3.141592653589793)*exp(-y[0]*y[0])*1.000000000070638;
+    result = exp((y[0]*y[0])*(y[0]*y[0]-2.0)*(-1.0/4.0))*2.560729512196574E-1;
     return result;
 }
 
 vector< vector<double> > Problem::day(vector<double> x, vector<double> y) {
     vector< vector<double> > result(this->d,vector<double>(this->nf,0.));
-    result[0][0] = cos(x[0])*(cos(y[0])*3.0-y[0]*sin(y[0])*2.0);
+    result[0][0] = cos(x[0])*((y[0]*y[0])*cos(y[0])*3.0-(y[0]*y[0]*y[0])*sin(y[0])+y[0]*sin(y[0]));
     return result;
 }
 
 vector<double> Problem::drif(vector<double> x, vector<double> y) {
     vector<double> result(2*this->nf,0.);
-    result[0] = y[0]*2.0;
-    result[1] = cos(x[0])*cos(y[0])-y[0]*y[1]*2.0;
+    result[0] = -y[0]+y[0]*y[0]*y[0];
+    result[1] = cos(x[0])*cos(y[0])+y[1]*(y[0]-y[0]*y[0]*y[0]);
     return result;
 }
 
