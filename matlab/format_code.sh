37f3e7eb2cf32f98a0e4dec9dd3dde4289ccf4b5
Urbain Vaes <urbain@vaes.uk>
2015-06-29 14:22:41 +0200

    Modify MATLAB code to be compatible with C++

--- matlab/format_code.sh
+++ matlab/format_code.sh
@@ -8,10 +8,10 @@ done
 dou="v.sub lin.sub rho.sub hy.sub"
 vec="vy.sub g.sub f.sub h.sub drif.sub diff.sub"
 mat="gx.sub fx.sub fy.sub"
-spl="fsplit.sub fxsplit.sub"
+spl="split.sub"
 
 # Formatting for double, vector, and matrix outputs
-sed -i 's/^  .*=/    return /g' $spl
+sed -i 's/^  [^ ]* =/    return/g' $spl
 sed -i 's/^  [^=]*/    result /g' $dou
 sed -i 's/^  [^\[]*\[[0-9]\{1,2\}\]\(\[[0-9]\{1,2\}\]\)/    result\1/g' $vec
 sed -i 's/^  [^\[]*\(\[[0-9]\{1,2\}\]\)\(\[[0-9]\{1,2\}\]\)/    result\1\2/g' $mat
@@ -19,7 +19,7 @@ sed -i 's/^  [^\[]*\(\[[0-9]\{1,2\}\]\)\(\[[0-9]\{1,2\}\]\)/    result\1\2/g' $m
 # Fix of the case where there is only 1 slow process
 sed -i 's/^  [^\[]* =/    result[0] =/g' $vec
 sed -i 's/^  [^\[]* =/    result[0][0] =/g' $mat
-
+   
 # Matching c++ program variables
 sed -i 's/y\(\([0-9]\|[0-9][0-9]\)\)/y\[\1\]/g' *.sub
 sed -i 's/x\(\([0-9]\|[0-9][0-9]\)\)/x\[\1\]/g' *.sub
