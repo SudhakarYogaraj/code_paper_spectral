6d3fc258c34618efc7c3c9b40db75b03635c226a
Urbain Vaes <u.vaes13@imperial.ac.uk>
2015-07-04 16:27:52 +0200

    Correct bug
    There were 2 bugs:
    
    - One was in the exact solution.
    
    - The other one was a python oddity: [[0]*n]*n creates a nxn matrix, but
      all the lines of the matrix point to the same list, and so the stay
      identical.
    
    Now, the code works with the simple 2x2 problem.

--- python/build_problem.py
+++ python/build_problem.py
@@ -28,9 +28,9 @@ g = [0.]*ns
 f = [0.]*ns
 h = [0.]*nf
 vy = [0.]*nf
-hy = [[0.]*nf]*nf
-gx = [[0.]*nf]*nf
-fx = [[0.]*nf]*nf
+hy = [[0.]*nf for i in range(nf)]
+gx = [[0.]*ns for i in range(ns)]
+fx = [[0.]*ns for i in range(ns)]
 
 v = 0.5 * ((y[0] - 1) ** 4 + (y[1] - 1) ** 2 + 0.2*(y[0] - 1) * (y[1] - 1))
 
@@ -42,10 +42,10 @@ g[0] = sympy.cos(x[0]) * sympy.sin(y[0]*y[1])
 g[1] = sympy.cos(x[0] + x[1]) * sympy.sin(y[0] + y[1])
 
 # Non-leading order drift of fast process
-h[0] = sympy.Symbol('0')
-h[1] = sympy.Symbol('0')
-# h[0] = sympy.cos(x[0]) * sympy.cos(y[0]) * sympy.cos(y[1])
-# h[1] = sympy.cos(x[0]) * sympy.cos(y[0] + y[1])
+h[0] = sympy.cos(x[0]) * sympy.cos(y[0]) * sympy.cos(y[1])
+h[1] = sympy.cos(x[0]) * sympy.cos(y[0] + y[1])
+# h[0] = sympy.Symbol('0')
+# h[1] = sympy.Symbol('0')
 
 # DEPENDENT VARIABLES
 
@@ -71,7 +71,7 @@ elif nf == 3:
 # Right-hand side of Poisson equation
 for i in range(ns):
     for j in range(nf):
-        f[i] += 0.5 * sympy.diff(S * rho * sympy.diff(g[i], y[j]), y[j]) / rho
+        f[i] -= 0.5 * sympy.diff(S * rho * sympy.diff(g[i], y[j]), y[j]) / rho
         f[i] = sympy.simplify(f[i])
 
 # Linear term
@@ -82,8 +82,13 @@ for i in range(nf):
 
 # Derivative of h
 for i in range(nf):
+    print h[i]
     for j in range(nf):
         hy[i][j] = sympy.diff(h[i], y[j])
+        print hy[i][j]
+        print hy
+
+print hy
 
 # differential of g
 for i in range(ns):
@@ -101,11 +106,10 @@ for i in range(nf):
     stardivh += vy[i] * h[i] - hy[i][i]
 stardivh = sympy.simplify(stardivh)
 
-
 # VARIABLES FOR THE HMM
 
-vyy = [[0.] * nf] * nf
-fy = [[0.] * ns] * nf
+vyy = [[0.]*nf for i in range(nf)]
+fy = [[0.]*nf for i in range(ns)]
 drif = [0.] * (2*nf)
 diff = [0.] * (2*nf)
 
