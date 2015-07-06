87cee131d5d71170ddb5a27648a54403b7ce7a3b
Urbain Vaes <urbain@vaes.uk>
2015-07-06 18:03:23 +0200

    Create subdirectory for C++ code

--- python/build_problem.py
+++ python/build_problem.py
@@ -15,7 +15,7 @@ os.system("mkdir -p tmp")
 
 # USER INPUT: Number of slow and fast variables
 ns = 1
-nf = 2
+nf = 1
 # END OF USER INPUT
 
 # Coefficient of the BM
@@ -48,8 +48,9 @@ drif = [0.] * (2*nf)
 diff = [0.] * (2*nf)
 
 # USER INPUT: solution of the cell problem
-g[0] = sympy.cos(x[0]) * sympy.cos(y[0] + y[1]) - y[0] ** 2 - y[1] ** 2 + y[0]*y[1]
+# g[0] = sympy.cos(x[0]) * sympy.cos(y[0] + y[1]) - y[0] ** 2 - y[1] ** 2 + y[0]*y[1]
 # g[0] = sympy.cos(x[0]) * sympy.cos(y[0]**2 + y[1] ** 2) - y[0] ** 2 - y[1] ** 2
+g[0] = sympy.cos(x[0] + y[0])
 # g[0] = -x[0] * (y[0] ** 5 * y[1] ** 6)
 # g[1] = sympy.sin(x[0]) * sympy.sin(y[0] + y[1])
 # g[1] = sympy.cos(x[0] + x[1]) * sympy.sin(y[0] + y[1])
@@ -60,8 +61,8 @@ for i in range(ns):
         gx[i][j] = sympy.diff(g[i], x[j])
 
 # USER INPUT: Non-leading order drift of fast process
-h[0] = sympy.cos(x[0]) * sympy.cos(y[0]) * sympy.cos(y[1])
-h[1] = sympy.cos(x[0]) * sympy.cos(y[0] + y[1])
+h[0] = sympy.cos(x[0]) * sympy.cos(y[0]) * sympy.exp(y[0])
+# h[1] = sympy.cos(x[0]) * sympy.cos(y[0] + y[1])
 # h[0] = sympy.Symbol('0')
 # h[1] = sympy.Symbol('0')
 # END OF USER INPUT
@@ -72,8 +73,8 @@ for i in range(nf):
 stardivh = sympy.simplify(stardivh)
 
 # USER INPUT: Potential
-v = 0.5 * ((y[0] - 1) ** 4 + (y[1] - 1) ** 2 + 0.2*(y[0] - 1) * (y[1] - 1))
-# v = 0.5 * (y[0] - 1) ** 2
+# v = 0.5 * ((y[0] - 1) ** 4 + (y[1] - 1) ** 2 + 0.2*(y[0] - 1) * (y[1] - 1))
+v = 0.5 * (y[0] - 1) ** 2
 # END OF USER INPUT
 for i in range(nf):
     vy[i] = sympy.diff(v, y[i])
