4486235635662c659268e9d48f66e3aca0a26d9f
Urbain Vaes <urbain@vaes.uk>
2015-10-23 16:28:36 +0100

    Add tests for solvers in separate files

---
+++ src/tests/error_spectral.hpp
@@ -0,0 +1,13 @@
+#ifndef ERROR_SPECTRAL_H
+#define ERROR_SPECTRAL_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+
+namespace tests {
+    void error_spectral(std::vector<double> x, Problem*, Analyser*);
+}
+
+#endif
