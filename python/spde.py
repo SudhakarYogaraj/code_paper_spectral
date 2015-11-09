4909c887cdc38f2a57f28d99fd4d5abb2def2699
Urbain Vaes <urbain@vaes.uk>
2015-11-09 15:26:15 +0000

    Extracting linear system from spde

--- python/spde.py
+++ python/spde.py
@@ -1,19 +1,23 @@
-import numpy
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sympy
+import numpy
 
 # Boundaries of the domain.
-L = -numpy.pi
-R = numpy.pi
+L = -sympy.pi
+R = sympy.pi
 
 # Dimension of the kernel of the differential operator.
-d = 2
+ns = 2
 
-# Truncation index.
-N = 6
+# Number of fast modes to keep
+nf = 4
 
 # Degree of the nonlinearity
 degree = 2
 
+# Space variable
 x = sympy.Symbol('x')
 
 # Differential operator associated with the equation.
@@ -21,37 +25,55 @@ def operator(function):
     return sympy.diff(function, x, 2) + function
     # return -sympy.diff(u,x,4) - sympy.diff(u,x,2)
 
-# Normalized eigenfunctions of the differential operator.
-def eigenfunctions(i):
-    if i%2 == 0:
-        return sympy.cos(i/2 * x) * numpy.sqrt(1/numpy.pi)
-    else:
-        return sympy.sin((i+1)/2 * x) * numpy.sqrt(1/numpy.pi)
 
-# Functions used in the expansion.
-def expa(i):
-    if i%2 == 0:
-        return sympy.cos(i/2 * x) * numpy.sqrt(1/numpy.pi)
+# Normalized eigenfunctions of the differential operator. (i = 1, 2 ...)
+def eigenfunctions(i):
+    if (i + 1) % 2 == 0:
+        return sympy.cos(sympy.Integer((i+1)/2) * x) * sympy.sqrt(1/sympy.pi)
     else:
-        return sympy.sin((i+1)/2 * x) * numpy.sqrt(1/numpy.pi)
+        return sympy.sin(sympy.Integer((i+2)/2) * x) * sympy.sqrt(1/sympy.pi)
+
+
+# Functions used in the expansion. (i = 1, 2 ...)
+def expa(i):
+    if (i + 1) % 2 == 0:
+        return sympy.cos(sympy.Integer((i+1)/2) * x)
+    else:
+        return sympy.sin(sympy.Integer((i+2)/2) * x)
+
 
 # Nonlinearity.
-def nonlin(f):
-    return 0.5 * sympy.diff(f*f, x, 1)
+def nonlinearity(f):
+    return sympy.Rational(0.5) * sympy.diff(f*f, x, 1)
 
-## End of user input ##
+# END OF USER INPUT
 
-# Projection of the nonlinearity on the eigenspaces
+# Coefficients of the expansion
+coefficients = [sympy.Symbol('c%d' % i) for i in range(ns + nf)]
 
-    fs = sym(zeros(1,N));
-    es = sym(zeros(1,N));
-    u  = sym(0);
-    for i = 1:N;
-        fs(i) = eigfun(i,x);
-        es(i) = expa(i,x);
-        u = u + xs(i)*es(i);
-    end
+# Space variable
+x = sympy.Symbol('x')
+u = 0
 
-    nonLin = simplify(F(u));
-    toInt  = simplify(expand(nonLin*transpose(fs)));
-    tic; proj = simplify(int(toInt, x, L, R)/sqrt(pi)); toc
+exp = []
+eig = []
+
+for i in range(ns + nf):
+    exp.append(expa(i))
+    eig.append(eigenfunctions(i))
+    u += coefficients[i] * exp[i]
+
+# Calculation of the nonlinearity
+nonlinear_term = sympy.collect(sympy.expand(sympy.expand_trig(nonlinearity(u))), [sympy.sin(x), sympy.cos(x)])
+
+print("Nonlinearity:")
+print(nonlinear_term)
+
+# Projection of the nonlinearity
+# TODO: justify division by sqrt(pi)
+projections = []
+for i in range(ns + nf):
+    product = sympy.expand(nonlinear_term * sympy.expand_trig(eig[i]))
+    result_integration = sympy.integrate(product, (x,L,R)) / sympy.sqrt(sympy.pi)
+    projections.append(sympy.simplify(result_integration))
+    print(projections[i])
