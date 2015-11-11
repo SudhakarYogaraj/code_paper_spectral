26e809ee5a24f7db91e1be1e1f356034eb71a713
Urbain Vaes <urbain@vaes.uk>
2015-11-11 12:22:41 +0000

    Add diffusion coefficient as input

--- 1D_easy.py
+++ 1D_easy.py
@@ -1,20 +1,27 @@
 # Description of the variables
-
+#
 # ns : number of slow processes
 # nf : number of fast processes
-
+#
 # g : solution of the cell problem (dimension = ns)
-
+#
 # h : Non-leading order part of the drift of the fast processes.
 # (The one that multiplies 1/eps.)
-
+#
 # v : The potential of the problem
+#
+# s : Diffusion coefficient
+#
 
 ## 1D problem
 
 # user input : dimensions
 ns = 1
 nf = 1
+# end
+
+# user input : diffusion
+s = sympy.sqrt(2)
 # end
 
 # user input : solution
